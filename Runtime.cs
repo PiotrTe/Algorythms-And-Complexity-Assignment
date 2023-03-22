4d0bd127a4cd5aae37702f859c340d5d97c1f5e5
PiotrTe <piotrekteter@gmail.com>
2023-03-22 08:28:14 +0000

    Added both searching algorithms

--- Runtime.cs
+++ Runtime.cs
@@ -12,7 +12,9 @@ namespace AlgorythmsAndComplexity
         {
             Roads roads = new Roads();
             Sorting sorting = new Sorting();
-            Dictionary<string, int[]> workingFile = null;
+            Search search = new Search();
+            Dictionary<string, int[]>? workingFile = null;
+            int[]? workingFile2 = null;
             while (true)
             {
                 int userInput = GetIntInput("Choose to sort one of the following files:\n[1] Road1-256\n[2] Road1-2048\n[3] Road2-256\n[4] Road2-2048\n[5] Road3-256\n[6] Road3-2048\n[7] Road4-512\n[8] Road4-4096\n[0] Exit",0,8);
@@ -27,16 +29,41 @@ namespace AlgorythmsAndComplexity
                 else if (userInput == 0) break;
 
                 sorting.SortArray(workingFile, GetIntInput("Choose one of the sorting methods:\n[1] Insertion Sort\n[2] Bubble sort\n[3] Quick Sort\n[4] Merge Sort",1,4));
-                userInput = GetIntInput("Show Asceding [1] or Descending [2] order?",0,8);
+                userInput = GetIntInput("Show Asceding [1] or Descending [2] order?",1,2);
                 if (userInput == 1)
                 {
+                    workingFile2 = workingFile["Ascending"];
                     sorting.OutputElements(workingFile["Ascending"],workingFile["Identifier"]);
                 }
                 else if (userInput == 2)
                 {
+                    workingFile2 = workingFile["Descending"];
                     sorting.OutputElements(workingFile["Descending"],workingFile["Identifier"]);
                 }
                 
+                userInput = GetIntInput("Choose one of the searching methods:\n[1] Linear Search\n[2] Binary Search",1,2);
+                if (userInput == 1)
+                {
+                    Dictionary<int, int> results = Search.LinearSearch(workingFile2,GetIntInput("Enter the number you wish to search for: ",0,workingFile2.Length));
+                    string resultsString = "";
+                    foreach (KeyValuePair<int, int> position in results)
+                    {
+                        resultsString += ($"Value {position.Value} at position {position.Key}\n");
+                    }
+                    System.Console.WriteLine($"Values found in following positions: \n{resultsString}");
+                }
+                else if (userInput == 2)
+                {
+                    Dictionary<int, int> results = Search.BinarySearch(workingFile2,GetIntInput("Enter the number you wish to search for: ",0,workingFile2.Length));
+                    System.Console.WriteLine($"\nBinary Search completed in {Search.Steps} steps");  
+                    Search.Steps = 0;
+                    string resultsString = "";
+                    foreach (KeyValuePair<int, int> position in results)
+                    {
+                        resultsString += ($"Value {position.Value} at position {position.Key}\n");
+                    }
+                    System.Console.WriteLine($"Values found in following positions: \n{resultsString}");
+                }
             }
         }
     }
---
+++ Search.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorythmsAndComplexity
+{
+    class Search
+    {
+        static public int Steps = 0;
+        public static Dictionary<int, int> LinearSearch(int[] array, int target)
+        {
+            Steps = 0;
+            int n = array.Length;
+            Dictionary<int,int> positions = new Dictionary<int, int>();
+            for (int i = 0; i < n; i++)
+            {
+                if (array[i] == target)
+                    positions.Add(i,target);
+                    Steps++;
+            }
+            int targetL = target;
+            int targetR = target;
+            if (positions.Count == 0){
+                System.Console.WriteLine($"\nValue {target} not found... Looking for closest values");
+                while (positions.Count == 0)
+                {
+                    targetL --;
+                    targetR ++;
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (array[i] == targetL)
+                            positions.Add(i,targetL);
+                            Steps++;
+                    }
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (array[i] == targetR)
+                            positions.Add(i, targetR);
+                            Steps++;
+                    }
+                }
+            }
+            System.Console.WriteLine($"\nLinear Search completed in {Steps} Steps");    
+            return positions;
+
+        }
+
+        public static Dictionary<int,int> BinarySearch(int[] array, int target)
+        {
+            Dictionary<int,int> result = new Dictionary<int,int>();
+            int left = 0;
+            int right = array.Length - 1;
+            while (left <= right)
+            {
+                Steps++;
+                int mid = left + (right - left) / 2;
+                if (array[mid] == target)
+                {
+                    result.Add(mid,target);
+                    // search for other occurrences on the left side
+                    int i = mid - 1;
+                    while (i >= 0 && array[i] == target)
+                    {
+                        result.Add(i,target);
+                        i--;
+                        Steps++;
+                    }
+                    // search for other occurrences on the right side
+                    i = mid + 1;
+                    while (i < array.Length && array[i] == target)
+                    {
+                        result.Add(i,target);
+                        i++;
+                        Steps++;
+                    }
+                    break;
+                }
+                else if (array[mid] < target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            if (result.Count == 0)
+            {
+                System.Console.WriteLine($"\nValue {target} not found... Looking for closest values");
+                // find closest values
+                int targetL = target - 1;
+                int targetR = target + 1;
+
+                while (result.Count == 0 && (targetL >= array[0] || targetR <= array[array.Length - 1]))
+                {
+                    Steps++;
+
+                    // search for closest value on the left side
+                    if(targetL >= array[0])
+                    {
+                        var resL=BinarySearch(array, targetL);
+                        foreach(var item in resL){
+                            result.Add(item.Key,item.Value);
+                        }
+                    }
+
+                    // search for closest value on the right side
+                    if(targetR <= array[array.Length - 1])
+                    {
+                        var resR=BinarySearch(array, targetR);
+                        foreach(var item in resR){
+                            result.Add(item.Key,item.Value);
+                        }
+                    }
+
+                    // update targets for next iteration
+                    targetL--;
+                    targetR++;
+                }
+            }
+            return result;
+        }
+    }
+}
